fae93898672ebd8eb4311eba6300e3baa8f50251
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-09-25 00:14:01 +0700

    Updates

---
+++ precache-manifest.62800983a9e21dce544717ab955ce783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5349ecabb3210dc1f6fc699faee3916e",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "5fd2270bafc859e44c06",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e10131e1587f687f514b",
+    "url": "/reactapp/static/js/2.b3dc571a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.b3dc571a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd2270bafc859e44c06",
+    "url": "/reactapp/static/js/main.3ee8b37b.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
